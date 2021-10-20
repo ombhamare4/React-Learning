5658f28545ebc155e17393b74488f0921b144526
omrubiks <om.rubiks@gmail.com>
2021-10-20 18:00:27 +0530

    Commit

--- MERN-Full-App-1/server/app.js
+++ MERN-Full-App-1/server/app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+const DB = "mongodb+srv://webRubik:test'OR'a'='a'@web-db.qjovo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+mongoose.connect(DB);
+
+const middleware =(req,res,next)=>{
+    console.log("Hello My Middleware");
+    next();
+}
+// middleware();
 
 app.get('/',(req, res) => {
     res.send("Hello World");
+});
+
+app.get('/about',middleware,(req, res) => {
+    res.send("About you?");
 });
 
 let port = 8000;
