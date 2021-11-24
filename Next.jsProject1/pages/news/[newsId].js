95203ec69b726c976bfc91528c2b799f4e10c0a4
omrubiks <om.rubiks@gmail.com>
2021-11-24 08:11:42 +0530

    Commit

--- Next.jsProject1/pages/news/[newsId].js
+++ Next.jsProject1/pages/news/[newsId].js
@@ -1,6 +1,10 @@
-
+import {useRouter} from 'next/router'
 const DetailPage = () =>{
+  
+  const route = useRouter();
+  console.log(route.query.newsId);
     return(
+
       <h1>detail Page</h1>
     )
   };
